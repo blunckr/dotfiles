4871c32d7c20cf00127e46e6cdf8e7662215a084
Ryker Blunck <rykerblunck@gmail.com>
2016-12-21 22:31:33 -0700

    better sync

--- sync.sh
+++ sync.sh
@@ -1,10 +1,10 @@
 # --dry-run for rsync
 find ~/Movies -type f -name ".*" -delete
-rsync -ai --delete --size-only ~/Movies/ /Volumes/TERRA/movies
+rsync -ai --delete --size-only ~/Movies/ pi@bluncks.duckdns.org:/mnt/movies --iconv=utf-8-mac,utf-8
 find ~/better_music -type f -name ".*" -delete
-rsync -ai --delete --size-only ~/better_music/ /Volumes/TERRA/music
+rsync -ai --delete --size-only ~/better_music/ pi@bluncks.duckdns.org:/mnt/music --iconv=utf-8-mac,utf-8
 find ~/Pictures/Photos\ Library.photoslibrary/Masters -type f -name ".*" -delete
-rsync -ai --delete --size-only ~/Pictures/Photos\ Library.photoslibrary/Masters/ /Volumes/TERRA/photos
+rsync -ai --delete --size-only ~/Pictures/Photos\ Library.photoslibrary/Masters/ pi@bluncks.duckdns.org:/mnt/pictures --iconv=utf-8-mac,utf-8
 
 # sudo find /Users/rykerblunck/Library/Application\ Support/OpenEmu/Game\ Library/roms -type f -name ".*"
 # sudo rsync -aEi --delete --size-only /Users/rykerblunck/Library/Application\ Support/OpenEmu/Game\ Library/roms/ /Volumes/TERRA/games
