1550e556900404cafc1378da6dfe7c4fed724eb8
Ryker Blunck <rykerblunck@gmail.com>
2018-02-20 16:27:05 -0700

    control pianobar

--- music.sh
+++ music.sh
@@ -7,4 +7,10 @@ then
         play) cmus-remote -u;;
         next) cmus-remote -n;;
     esac
+elif pidof pianobar
+then
+    case $1 in
+        play) echo -n 'p' > ~/.config/pianobar/ctl;;
+        next) echo -n 'n' > ~/.config/pianobar/ctl;;
+    esac
 fi
